740ed2daabb2739782bf967078c84d5adabb4423
ZainAL-Ajjori <zainajjori@gimal.com>
2026-08-30 16:31:18 +0300

    refactor: align FindFewestHopsRouteUseCase to use graph builder pattern

--- src/main/kotlin/org/bytebloom/domain/usecase/required/FindFewestHopsRouteUseCase.kt
+++ src/main/kotlin/org/bytebloom/domain/usecase/required/FindFewestHopsRouteUseCase.kt
@@ -1,13 +1,23 @@
-package org.bytebloom.domain.usecase.required
+package org.bytebloom.domain.usecase
 
 import org.bytebloom.domain.model.Warehouse
+import org.bytebloom.domain.repository.RouteRepository
+import org.bytebloom.domain.repository.WarehouseRepository
+import org.bytebloom.domain.routing.WarehouseGraphBuilder
 import org.bytebloom.domain.routing.bfs.BidirectionalBreadthFirstRouter
 
 class FindFewestHopsRouteUseCase(
-    private val bfsRouter: BidirectionalBreadthFirstRouter
+    private val warehouseRepository: WarehouseRepository,
+    private val routeRepository: RouteRepository
 ) {
 
     operator fun invoke(startWarehouse: Warehouse, endWarehouse: Warehouse): List<Warehouse>? {
+        val warehouseGraph = WarehouseGraphBuilder(
+            warehouses = warehouseRepository.getAll(),
+            routes = routeRepository.getAll()
+        ).build()
+
+        val bfsRouter = BidirectionalBreadthFirstRouter(warehouseGraph)
         return bfsRouter.findShortestPath(startWarehouse, endWarehouse)
     }
 
