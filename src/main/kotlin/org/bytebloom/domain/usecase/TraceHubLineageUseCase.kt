82d3ea86dbc2cd829850ee4d322ade96700297b9
Abdallah-Jr05 <a421301946@gmail.com>
2026-08-31 12:17:20 +0300

    refactor: make TraceHubLineageUseCase simple

--- src/main/kotlin/org/bytebloom/domain/usecase/TraceHubLineageUseCase.kt
+++ src/main/kotlin/org/bytebloom/domain/usecase/TraceHubLineageUseCase.kt
@@ -6,12 +6,6 @@ import org.bytebloom.domain.tree.hierarchicalHub.HubTree
 class TraceHubLineageUseCase(
     private val hubTree: HubTree
 ) {
-    operator fun invoke(warehouse: Warehouse): List<Warehouse> {
-        val node = hubTree.findNode(warehouse)
-            ?: return emptyList()
-
-        return generateSequence(node) { it.parent }
-            .map { it.warehouse }
-            .toList()
-    }
+    operator fun invoke(warehouse: Warehouse): List<Warehouse> =
+        hubTree.lineageFrom(warehouse)
 }
