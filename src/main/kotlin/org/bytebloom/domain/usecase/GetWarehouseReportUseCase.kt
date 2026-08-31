3f91f703ff893737ca9310145a36786b8f8b0a20
Abdallah-Jr05 <a421301946@gmail.com>
2026-08-31 18:03:38 +0300

    refactor: improve GetWarehouseReportUseCase.kt

--- src/main/kotlin/org/bytebloom/domain/usecase/GetWarehouseReportUseCase.kt
+++ src/main/kotlin/org/bytebloom/domain/usecase/GetWarehouseReportUseCase.kt
@@ -3,6 +3,8 @@ package org.bytebloom.domain.usecase
 import org.bytebloom.domain.repository.WarehouseRepository
 import org.bytebloom.domain.repository.PackageRepository
 import org.bytebloom.domain.repository.VehicleRepository
+import org.bytebloom.domain.model.Package
+import org.bytebloom.domain.model.Vehicle
 
 class GetWarehouseReportUseCase(
     private val warehouseRepository: WarehouseRepository,
@@ -10,30 +12,35 @@ class GetWarehouseReportUseCase(
     private val vehicleRepository: VehicleRepository
 ) {
 
-    operator fun invoke(warehouseId: String): WarehouseReport? {
+    operator fun invoke(
+        warehouseId: String
+    ): WarehouseReport? {
 
-        val warehouse = warehouseRepository.getAll()
-            .find { it.id == warehouseId }
-            ?: return null
-        val packages = packageRepository.getAll()
-            .filter { it.originWarehouse.id == warehouseId }
+        val warehouse =
+            warehouseRepository
+                .getAll()
+                .firstOrNull { it.id == warehouseId }
+                ?: return null
 
-        val vehicles = vehicleRepository.getAll()
-            .filter { it.currentWarehouse.id == warehouseId }
+        val packages =
+            packageRepository
+                .getAll()
+                .asSequence()
+                .filter { it.originWarehouse.id == warehouseId }
+                .toList()
 
-        val packageCount = packages.size
-
-        val totalPackageWeight =
-            packages.sumOf { it.weight }
-
-        val totalVehicleCapacity =
-            vehicles.sumOf { it.maxCapacityKg }
+        val vehicles =
+            vehicleRepository
+                .getAll()
+                .asSequence()
+                .filter { it.currentWarehouse.id == warehouseId }
+                .toList()
 
         return WarehouseReport(
             warehouseId = warehouse.id,
-            packageCount = packageCount,
-            totalPackageWeight = totalPackageWeight,
-            totalVehicleCapacity = totalVehicleCapacity
+            packageCount = packages.size,
+            totalPackageWeight = packages.sumOf(Package::weight),
+            totalVehicleCapacity = vehicles.sumOf(Vehicle::maxCapacityKg)
         )
     }
 }
