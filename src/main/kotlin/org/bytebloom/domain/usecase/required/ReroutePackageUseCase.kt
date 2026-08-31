432d05062854bfc02de76a140633a72fb1e144a7
Abdallah-Jr05 <a421301946@gmail.com>
2026-08-31 18:03:27 +0300

    refactor: improve ReroutePackageUseCase

--- src/main/kotlin/org/bytebloom/domain/usecase/required/ReroutePackageUseCase.kt
+++ src/main/kotlin/org/bytebloom/domain/usecase/required/ReroutePackageUseCase.kt
@@ -5,8 +5,8 @@ import org.bytebloom.domain.model.Warehouse
 
 
 class ReroutePackageUseCase {
-    operator fun invoke(pkg: Package, warehouse: Warehouse): Warehouse {
+    operator fun invoke(pkg: Package, warehouse: Warehouse): Package {
         pkg.destinationWarehouse = warehouse
-        return warehouse
+        return pkg
     }
 }
