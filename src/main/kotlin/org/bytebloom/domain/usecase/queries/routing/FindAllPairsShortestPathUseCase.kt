14b5465a5354978d76ce2c174530604da3b2e12c
Abdallah-Jr05 <a421301946@gmail.com>
2026-09-02 12:27:32 +0300

    refactor: improve FindAllPairsShortestPathUseCase

--- src/main/kotlin/org/bytebloom/domain/usecase/queries/routing/FindAllPairsShortestPathUseCase.kt
+++ src/main/kotlin/org/bytebloom/domain/usecase/queries/routing/FindAllPairsShortestPathUseCase.kt
@@ -2,101 +2,40 @@ package org.bytebloom.domain.usecase.queries.routing
 
 import org.bytebloom.domain.repository.RouteRepository
 import org.bytebloom.domain.model.Warehouse
+import org.bytebloom.domain.routing.WarehouseGraph
+import org.bytebloom.domain.routing.common.RouteFinder
 
 class FindAllPairsShortestPathUseCase(
-    private val routeRepository: RouteRepository
+    private val routeFinder: RouteFinder,
+    private val graph: WarehouseGraph
 ) {
-    operator fun invoke(warehouses: List<Warehouse>): Map<Warehouse, Map<Warehouse, Double>> {
-        val warehouseIndex = createWarehouseIndex(warehouses)
-        val distances = initializeDistances(warehouses.size)
-        addDirectRoutes(
-            distances,
-            warehouseIndex
-        )
 
-        calculateShortestPaths(
-            distances,
-            warehouses
-        )
+    operator fun invoke(): Map<Warehouse, Map<Warehouse, Double>> {
+        val warehouses = graph.warehouses()
 
-        return buildResult(
-            warehouses,
-            warehouseIndex,
-            distances
-        )
-
-    }
-
-    private fun createWarehouseIndex(
-        warehouses: List<Warehouse>
-    ): Map<String, Int> {
-        return warehouses.mapIndexed { index, warehouse ->
-            warehouse.id to index
-        }.toMap()
-    }
-
-    private fun initializeDistances(
-        warehousesCount: Int
-    ): MutableList<MutableList<Double>> {
-
-        val distances = MutableList(warehousesCount) {
-            MutableList(warehousesCount) {
-                Double.POSITIVE_INFINITY
-            }
-        }
-        distances.forEachIndexed { index, _ ->
-            distances[index][index] = 0.0
-        }
-        return distances
-    }
-
-    private fun addDirectRoutes(
-        distances: MutableList<MutableList<Double>>,
-        warehouseIndex: Map<String, Int>
-    ) {
-        val routes = routeRepository.getAll()
-        routes.forEach { route ->
-            val originIndex = warehouseIndex[route.originWarehouse.id]!!
-            val destinationIndex= warehouseIndex[route.destinationWarehouse.id]!!
-            distances[originIndex][destinationIndex] = route.distanceKm
-        }
-
-    }
-
-    private fun calculateShortestPaths(
-        distances: MutableList<MutableList<Double>>,
-        warehouses: List<Warehouse>
-        ) {
-        warehouses.indices.forEach { intermediateWarehouse ->
-            warehouses.indices.forEach { originWarehouse ->
-                warehouses.indices.forEach { destinationWarehouse ->
-                    distances[originWarehouse][destinationWarehouse] = minOf(
-                        distances[originWarehouse][destinationWarehouse],
-                        distances[originWarehouse][intermediateWarehouse] +
-                                distances[intermediateWarehouse][destinationWarehouse]
-
-                    )
+        return warehouses.associateWith { origin ->
+            warehouses.associateWith { destination ->
+                if (origin == destination) {
+                    0.0
+                } else {
+                    shortestDistance(origin, destination)
                 }
             }
         }
     }
 
+    private fun shortestDistance(
+        origin: Warehouse,
+        destination: Warehouse
+    ): Double {
+        val path = routeFinder.findShortestPath(origin, destination)
+            ?: return Double.POSITIVE_INFINITY
 
-    private fun buildResult(
-        warehouses: List<Warehouse>,
-        warehouseIndex: Map<String, Int>,
-        distances: List<List<Double>>
-    ): Map<Warehouse, Map<Warehouse, Double>> {
-        return warehouses.associate { originWarehouse ->
-            originWarehouse to warehouses.associate {destinationWarehouse ->
-                destinationWarehouse to distances[
-                    warehouseIndex[originWarehouse.id]!!
-                ][
-                    warehouseIndex[destinationWarehouse.id]!!
-                ]
+        return path
+            .zipWithNext()
+            .sumOf { (current, next) ->
+                graph.neighbors(current)[next]
+                    ?: Double.POSITIVE_INFINITY
             }
-        }
     }
-
-
 }
