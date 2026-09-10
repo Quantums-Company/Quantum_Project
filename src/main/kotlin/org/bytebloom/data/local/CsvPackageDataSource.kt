5b96d540954c357bd11bf035f70c4070e4d05ec6
Abdallah-Jr05 <a421301946@gmail.com>
2026-09-10 13:21:44 +0300

    feature: implement PackageDataSource

---
+++ src/main/kotlin/org/bytebloom/data/local/CsvPackageDataSource.kt
@@ -0,0 +1,11 @@
+package org.bytebloom.data.local
+
+import org.bytebloom.data.raw.PackageRaw
+import org.bytebloom.data.source.PackageDataSource
+
+class CsvPackageDataSource: PackageDataSource {
+
+    override fun loadAll(): List<PackageRaw> {
+        TODO("Not yet implemented")
+    }
+}
