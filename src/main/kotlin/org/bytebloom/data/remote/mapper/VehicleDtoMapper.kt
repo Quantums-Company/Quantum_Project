64225adc318ad7a56f6595a90c94d14cb450a655
ZainAL-Ajjori <zainajjori@gimal.com>
2026-09-14 18:47:08 +0300

    feat: add VehicleDtoMapper adapter to map vehicle DTOs to domain vehicles

--- src/main/kotlin/org/bytebloom/data/remote/mapper/VehicleDtoMapper.kt
+++ src/main/kotlin/org/bytebloom/data/remote/mapper/VehicleDtoMapper.kt
@@ -1,2 +1,28 @@
 package org.bytebloom.data.remote.mapper
 
+import org.bytebloom.data.remote.dto.VehicleDto
+import org.bytebloom.domain.model.Vehicle
+import org.bytebloom.domain.model.Warehouse
+
+object VehicleDtoMapper {
+
+    fun toDomain(
+        dto: VehicleDto,
+        warehousesById: Map<String, Warehouse>
+    ): Vehicle? {
+        val currentWarehouse = warehousesById[dto.currentWarehouseId] ?: return null
+
+        return Vehicle(
+            id = dto.id,
+            maxCapacityKg = dto.maxCapacityKg,
+            costPerKm = dto.costPerKm,
+            currentWarehouse = currentWarehouse
+        )
+    }
+
+    fun toDomainList(
+        dtos: List<VehicleDto>,
+        warehousesById: Map<String, Warehouse>
+    ): List<Vehicle> =
+        dtos.mapNotNull { toDomain(it, warehousesById) }
+}
