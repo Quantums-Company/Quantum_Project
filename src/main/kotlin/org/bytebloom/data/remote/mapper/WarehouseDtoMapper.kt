f14f8f72226efab99589253c1d1363c2e992af9c
ZainAL-Ajjori <zainajjori@gimal.com>
2026-09-14 18:46:17 +0300

    feat: add WarehouseDtoMapper adapter to convert remote DTOs to domain warehouses

--- src/main/kotlin/org/bytebloom/data/remote/mapper/WarehouseDtoMapper.kt
+++ src/main/kotlin/org/bytebloom/data/remote/mapper/WarehouseDtoMapper.kt
@@ -1,2 +1,19 @@
 package org.bytebloom.data.remote.mapper
 
+import org.bytebloom.data.remote.dto.WarehouseDto
+import org.bytebloom.domain.model.Warehouse
+
+object WarehouseDtoMapper {
+
+    fun toDomain(dto: WarehouseDto): Warehouse =
+        Warehouse(
+            id = dto.id,
+            name = dto.name,
+            regionalZone = dto.regionalZone,
+            longitude = dto.longitude,
+            latitude = dto.latitude
+        )
+
+    fun toDomainList(dtos: List<WarehouseDto>): List<Warehouse> =
+        dtos.map(::toDomain)
+}
