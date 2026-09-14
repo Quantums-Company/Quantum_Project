01033652db6b6c88a28a9657ec256108ddf67e8a
ZainAL-Ajjori <zainajjori@gimal.com>
2026-09-14 17:35:25 +0300

    feat: align Warehouse, Route, Package and Vehicle DTOs with Supabase column names via @SerialName

--- src/main/kotlin/org/bytebloom/data/remote/dto/PackageDto.kt
+++ src/main/kotlin/org/bytebloom/data/remote/dto/PackageDto.kt
@@ -1,9 +1,20 @@
 package org.bytebloom.data.remote.dto
 
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.SerialName
+
+
+@Serializable
+
 data class PackageDto(
+    @SerialName("id")
     val id: String = "",
+    @SerialName("weight")
     val weight: Double = 0.0,
+    @SerialName("originWarehouseId")
     val originWarehouseId: String = "",
+    @SerialName("destinationWarehouseId")
     val destinationWarehouseId: String = "",
+    @SerialName("priority")
     val priority: String = ""
 )
