ff4b258e4a8b146d52795f6b0ea2b87734f4baa0
Abdallah-Jr05 <a421301946@gmail.com>
2026-09-15 17:03:37 +0300

    refactor: handle the error to not appear in the console

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,6 +20,7 @@ repositories {
 dependencies {
     testImplementation(kotlin("test"))
     implementation("io.github.jan-tennert.supabase:supabase-kt:3.8.0")
+    implementation("org.slf4j:slf4j-simple:2.0.9")
     implementation(platform(libs.supabase.bom))
     implementation(libs.supabase.postgrest)
     implementation(libs.ktor.client.cio)
