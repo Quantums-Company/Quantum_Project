b698890f615ae50a840154528e9a18ca51f9b2e6
ZainAL-Ajjori <zainajjori@gimal.com>
2026-09-19 09:46:56 +0300

    fix(route): replace IllegalArgumentException with domain exceptions in CRUD use cases

--- src/main/kotlin/org/bytebloom/domain/usecase/crud/route/DeleteRouteUseCase.kt
+++ src/main/kotlin/org/bytebloom/domain/usecase/crud/route/DeleteRouteUseCase.kt
@@ -1,5 +1,6 @@
 package org.bytebloom.domain.usecase.crud.route
 
+import org.bytebloom.domain.exception.EntityValidationException
 import org.bytebloom.domain.repository.RouteRepository
 import org.bytebloom.domain.validator.RouteIdValidator
 import org.bytebloom.domain.validator.ValidationResult
@@ -9,16 +10,13 @@ class DeleteRouteUseCase(
     private val validator: RouteIdValidator
 ) {
     suspend operator fun invoke(id: String): Boolean {
-
         when (val result = validator(id)) {
             is ValidationResult.Valid -> {
                 return routeRepository.delete(id)
             }
 
             is ValidationResult.Invalid -> {
-                throw IllegalArgumentException(
-                    result.violations.joinToString(", ")
-                )
+                throw EntityValidationException(result.violations)
             }
         }
     }
--- src/main/kotlin/org/bytebloom/domain/usecase/crud/route/GetRouteByIdUseCase.kt
+++ src/main/kotlin/org/bytebloom/domain/usecase/crud/route/GetRouteByIdUseCase.kt
@@ -1,5 +1,6 @@
 package org.bytebloom.domain.usecase.crud.route
 
+import org.bytebloom.domain.exception.EntityValidationException
 import org.bytebloom.domain.model.Route
 import org.bytebloom.domain.repository.RouteRepository
 import org.bytebloom.domain.validator.RouteIdValidator
@@ -10,16 +11,13 @@ class GetRouteByIdUseCase(
     private val validator: RouteIdValidator
 ) {
     suspend operator fun invoke(id: String): Route? {
-
         when (val result = validator(id)) {
             is ValidationResult.Valid -> {
                 return routeRepository.getById(id)
             }
 
             is ValidationResult.Invalid -> {
-                throw IllegalArgumentException(
-                    result.violations.joinToString(", ")
-                )
+                throw EntityValidationException(result.violations)
             }
         }
     }
