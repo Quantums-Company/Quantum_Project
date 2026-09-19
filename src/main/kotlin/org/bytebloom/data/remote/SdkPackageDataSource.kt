0525cddce7807643abf43a5cadead1f2fb46887a
Abdallah-Jr05 <a421301946@gmail.com>
2026-09-19 11:24:48 +0300

    refactor: add validator to data source instead of repo

--- src/main/kotlin/org/bytebloom/data/remote/SdkPackageDataSource.kt
+++ src/main/kotlin/org/bytebloom/data/remote/SdkPackageDataSource.kt
@@ -2,6 +2,7 @@ package org.bytebloom.data.remote
 
 import io.github.jan.supabase.SupabaseClient
 import io.github.jan.supabase.postgrest.from
+import org.bytebloom.data.remote.client.SupabaseErrorTranslator
 import org.bytebloom.data.remote.dto.packageDto.PackageRequestDto
 import org.bytebloom.data.remote.dto.packageDto.PackageResponseDto
 import org.bytebloom.data.source.remote.PackageRemoteDataSource
@@ -11,25 +12,33 @@ class SdkPackageDataSource(
 ) : PackageRemoteDataSource {
 
     override suspend fun loadAll(): List<PackageResponseDto> =
-        client.from(TableName.PACKAGES).select().decodeList()
+        SupabaseErrorTranslator.translate("getAll packages") {
+            client.from(TableName.PACKAGES).select().decodeList()
+        }
 
     override suspend fun loadById(id: String): PackageResponseDto? =
-        client.from(TableName.PACKAGES)
-            .select { filter { eq("id", id) } }
-            .decodeSingleOrNull()
+        SupabaseErrorTranslator.translate("getById package '$id'") {
+            client.from(TableName.PACKAGES)
+                .select { filter { eq("id", id) } }
+                .decodeSingleOrNull()
+        }
 
-    override suspend fun create(request: PackageRequestDto): PackageResponseDto? {
-        client.from(TableName.PACKAGES).insert(request)
-        return loadById(request.id)
-    }
+    override suspend fun create(request: PackageRequestDto): PackageResponseDto? =
+        SupabaseErrorTranslator.translate("create package '${request.id}'") {
+            client.from(TableName.PACKAGES).insert(request)
+            loadById(request.id)
+        }
 
-    override suspend fun update(id: String, request: PackageRequestDto): PackageResponseDto? {
+
+    override suspend fun update(id: String, request: PackageRequestDto): PackageResponseDto? =
+        SupabaseErrorTranslator.translate("update package '${id}'") {
         client.from(TableName.PACKAGES).update(request) { filter { eq("id", id) } }
-        return loadById(id)
+        loadById(id)
     }
 
-    override suspend fun delete(id: String): Boolean {
+    override suspend fun delete(id: String): Boolean =
+        SupabaseErrorTranslator.translate("delete package '$id'") {
         client.from(TableName.PACKAGES).delete { filter { eq("id", id) } }
-        return true
+        true
     }
 }
--- src/main/kotlin/org/bytebloom/data/remote/SdkVehicleDataSource.kt
+++ src/main/kotlin/org/bytebloom/data/remote/SdkVehicleDataSource.kt
@@ -2,6 +2,7 @@ package org.bytebloom.data.remote
 
 import io.github.jan.supabase.SupabaseClient
 import io.github.jan.supabase.postgrest.from
+import org.bytebloom.data.remote.client.SupabaseErrorTranslator
 import org.bytebloom.data.remote.dto.vehicleDto.VehicleRequestDto
 import org.bytebloom.data.remote.dto.vehicleDto.VehicleResponseDto
 import org.bytebloom.data.source.remote.VehicleRemoteDataSource
@@ -11,25 +12,32 @@ class SdkVehicleDataSource(
 ) : VehicleRemoteDataSource {
 
     override suspend fun loadAll(): List<VehicleResponseDto> =
-        client.from(TableName.VEHICLES).select().decodeList()
+        SupabaseErrorTranslator.translate("getAll vehicles") {
+            client.from(TableName.VEHICLES).select().decodeList()
+        }
 
     override suspend fun loadById(id: String): VehicleResponseDto? =
-        client.from(TableName.VEHICLES)
-            .select { filter { eq("id", id) } }
-            .decodeSingleOrNull()
+        SupabaseErrorTranslator.translate("getById vehicle '$id'") {
+            client.from(TableName.VEHICLES)
+                .select { filter { eq("id", id) } }
+                .decodeSingleOrNull()
+        }
 
-    override suspend fun create(request: VehicleRequestDto): VehicleResponseDto? {
+    override suspend fun create(request: VehicleRequestDto): VehicleResponseDto? =
+        SupabaseErrorTranslator.translate("create vehicle '${request.id}'") {
         client.from(TableName.VEHICLES).insert(request)
-        return loadById(request.id)
+        loadById(request.id)
     }
 
-    override suspend fun update(id: String, request: VehicleRequestDto): VehicleResponseDto? {
+    override suspend fun update(id: String, request: VehicleRequestDto): VehicleResponseDto? =
+    SupabaseErrorTranslator.translate("update vehicle '${id}'") {
         client.from(TableName.VEHICLES).update(request) { filter { eq("id", id) } }
-        return loadById(id)
+        loadById(id)
     }
 
-    override suspend fun delete(id: String): Boolean {
+    override suspend fun delete(id: String): Boolean =
+        SupabaseErrorTranslator.translate("delete vehicle '$id'") {
         client.from(TableName.VEHICLES).delete { filter { eq("id", id) } }
-        return true
+        true
     }
 }
