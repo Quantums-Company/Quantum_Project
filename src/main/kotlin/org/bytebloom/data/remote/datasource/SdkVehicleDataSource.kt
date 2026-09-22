067518e70a43656d48bd75e09805c96f6ba799f3
ZainAL-Ajjori <zainajjori@gimal.com>
2026-09-22 09:45:45 +0300

    fix(retry): translate exceptions before retryWithBackoff sees them so network failures actually retry

--- src/main/kotlin/org/bytebloom/data/remote/datasource/SdkVehicleDataSource.kt
+++ src/main/kotlin/org/bytebloom/data/remote/datasource/SdkVehicleDataSource.kt
@@ -13,42 +13,42 @@ class SdkVehicleDataSource(
 ) : VehicleRemoteDataSource {
 
     override suspend fun loadAll(): List<VehicleResponseDto> =
-        SupabaseErrorTranslator.translate("getAll vehicles") {
-            retryWithBackoff {
+        retryWithBackoff {
+            SupabaseErrorTranslator.translate("getAll vehicles") {
                 client.from(TableName.VEHICLES).select().decodeList<VehicleResponseDto>()
-            }.getOrThrow()
-        }
+            }
+        }.getOrThrow()
 
     override suspend fun loadById(id: String): VehicleResponseDto? =
-        SupabaseErrorTranslator.translate("getById vehicle '$id'") {
-            retryWithBackoff {
+        retryWithBackoff {
+            SupabaseErrorTranslator.translate("getById vehicle '$id'") {
                 client.from(TableName.VEHICLES)
                     .select { filter { eq("id", id) } }
                     .decodeSingleOrNull<VehicleResponseDto>()
-            }.getOrThrow()
-        }
+            }
+        }.getOrThrow()
 
     override suspend fun create(request: VehicleRequestDto): VehicleResponseDto? =
-        SupabaseErrorTranslator.translate("create vehicle '${request.id}'") {
-            retryWithBackoff {
+        retryWithBackoff {
+            SupabaseErrorTranslator.translate("create vehicle '${request.id}'") {
                 client.from(TableName.VEHICLES).insert(request)
                 loadById(request.id)
-            }.getOrThrow()
-        }
+            }
+        }.getOrThrow()
 
     override suspend fun update(id: String, request: VehicleRequestDto): VehicleResponseDto? =
-        SupabaseErrorTranslator.translate("update vehicle '$id'") {
-            retryWithBackoff {
+        retryWithBackoff {
+            SupabaseErrorTranslator.translate("update vehicle '$id'") {
                 client.from(TableName.VEHICLES).update(request) { filter { eq("id", id) } }
                 loadById(id)
-            }.getOrThrow()
-        }
+            }
+        }.getOrThrow()
 
     override suspend fun delete(id: String): Boolean =
-        SupabaseErrorTranslator.translate("delete vehicle '$id'") {
-            retryWithBackoff {
+        retryWithBackoff {
+            SupabaseErrorTranslator.translate("delete vehicle '$id'") {
                 client.from(TableName.VEHICLES).delete { filter { eq("id", id) } }
                 true
-            }.getOrThrow()
-        }
+            }
+        }.getOrThrow()
 }
